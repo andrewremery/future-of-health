a34a5d2a7b503236d9d53ec6b67aedb4641bd5fc
Andrew R Emery <andrew@arevisual.com>
2019-09-29 16:48:58 -0400

    Changed Minsky Memorial Page to a Photo Gallery

---
+++ photos-from-the-future-of-health-technology-annual-summit.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+<!--Meta -->
+    <meta charset="utf-8">
+    <title>Future of Health Technology Institute Annual Summit Photo Gallery</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="description" content=" A loving memorial to Professor Marvin Minsky commemorating his life and work.">
+	<meta name="keywords" content="Future of Health Technology Institute summit">
+	<meta name="author" content="Andrew R Emery (ARE Visual)">
+    <link rel="shortcut icon" href="favicon.ico"/>
+<!--typekit-->
+    <script src="https://use.typekit.net/gvp3tyx.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!--CSS -->
+    <link rel="stylesheet" type="text/css" href="_css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="font-awesome-4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="_css/charles.css">
+    <link rel="stylesheet" type="text/css" href="_css/main.css">
+   
+<!--JS -->
+    <script src="_js/jquery.js"></script>
+    <script src="_js/bootstrap.min.js"></script>
+    <script src="_js/myScripts.js"></script>
+</head>
+<body>
+<?php include ("_inc/header.php");?>
+
+<!-- Photo Grid -->
+<section class="photo-gallery">
+<div class="row"> 
+  <div class="column">
+    <img src="_img/2019-summit/1.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/4.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/7.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/10.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/13.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/16.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+  </div>
+  <div class="column">
+    <img src="_img/2019-summit/2.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/5.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/8.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/11.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/14.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/17.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+  </div>  
+  <div class="column">
+    <img src="_img/2019-summit/3.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/6.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/9.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/12.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/15.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+    <img src="_img/2019-summit/18.jpg" style="width:100%" alt="Photo from the 2019 Future of Health Technology Institute Summit">
+  </div>
+</div>
+</section>
+
+</body>
+</html>
+
+<?php include ("_inc/footer.php");?>
+</body>
+</html>
+
+</html>
